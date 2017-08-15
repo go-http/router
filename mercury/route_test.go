e62bd3699e6ad7a96dd2590ced98d8ac14048f0d
Athurg Feng <athurg@gooth.org>
2017-08-15 17:56:29 +0800

    更新测试文件

--- mercury/route_test.go
+++ mercury/route_test.go
@@ -1,26 +1,36 @@
 package router
 
 import (
+	"net/url"
 	"os"
-	"strconv"
 	"testing"
+	"time"
 )
 
 func createClient(t *testing.T) *Client {
-	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	client, err := New(os.Getenv("USER"), os.Getenv("PASS"), os.Getenv("HOST"), port)
+	address := os.Getenv("ADDR")
+	if address == "" {
+		t.Fatal("ADDR参数不能为空")
+	}
+
+	addr, _ := url.Parse(address)
+	client, err := New(addr)
 	if err != nil {
-		t.Log(err)
-		os.Exit(1)
+		t.Fatal(err)
 	}
 
 	return client
 }
 
-func ExampleGetWlanStation(t *testing.T) {
+func TestGetWlanStation(t *testing.T) {
 	hosts, err := createClient(t).GetWlanStation()
 	if err != nil {
 		t.Error(err)
+		return
+	}
+
+	if hosts == nil {
+		return
 	}
 
 	t.Log(" \t无线主机MAC地址  \t当前状态\t包下载\t包上传")
@@ -33,6 +43,7 @@ func TestGetAllHostTraffic(t *testing.T) {
 	hosts, err := createClient(t).GetAllHostTraffic()
 	if err != nil {
 		t.Error(err)
+		return
 	}
 
 	t.Log(" \t当前IP地址\t流量(收/发)\t\t速度(收/发)")
@@ -45,9 +56,16 @@ func TestGetAssignedIpList(t *testing.T) {
 	hosts, err := createClient(t).GetAssignedIpList()
 	if err != nil {
 		t.Error(err)
+		return
 	}
 	t.Log("   客户端名称               MAC地址            IP地址     过期时间")
 	for i, host := range hosts {
 		t.Logf("%2d %-20s %s   %-13s %s\n", i+1, host.Name, host.Mac, host.Ip, host.IpExpireAt.Format("15:04:05"))
 	}
+}
+
+func TestGetStatus(t *testing.T) {
+	t1 := time.Now()
+	t1 = t1.AddDate(-1, 0, 0)
+	t.Log(createClient(t).SetDateTimeCfg(t1, nil, true))
 }
